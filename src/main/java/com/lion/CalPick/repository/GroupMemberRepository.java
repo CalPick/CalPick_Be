efd7faffeb093e74b0088f5bd7e5c68d40f639b1
AYEOOON <k93853471@gmail.com>
2025-07-16 21:16:54 +0900

    feat: 특정 userId에 속한 모든 그룹 멤버 정보를 반환하는 GroupMemberRepository 추가

---
+++ src/main/java/com/lion/CalPick/repository/GroupMemberRepository.java
@@ -0,0 +1,9 @@
+package com.lion.CalPick.repository;
+
+import com.lion.CalPick.domain.GroupMember;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMemberRepository extends JpaRepository<GroupMember, String> {
+    List<GroupMember> findByUserId(String userId);
+}
