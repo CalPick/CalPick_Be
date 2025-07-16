6d245d84b929f30e1f0933a6e3decda9948a3f44
AYEOOON <k93853471@gmail.com>
2025-07-16 21:17:25 +0900

    feat: GroupRepository 추가

---
+++ src/main/java/com/lion/CalPick/repository/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.lion.CalPick.repository;
+
+
+import com.lion.CalPick.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
