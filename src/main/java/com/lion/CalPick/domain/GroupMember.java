719545f1d3d2d1bbc4939c7f05233f7d8b15a804
AYEOOON <k93853471@gmail.com>
2025-07-16 21:13:06 +0900

    feat: 그룹과 그룹 내 사용자를 저장하는 GroupMember 엔티티 추가

---
+++ src/main/java/com/lion/CalPick/domain/GroupMember.java
@@ -0,0 +1,42 @@
+package com.lion.CalPick.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "group_members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FriendStatus status;
+}
